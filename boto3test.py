0ddac29967e6ef4567f5b7473378d7869310fc59
ishan <ishan747@uab.edu>
2019-06-18 14:03:16 -0500

    s3-upload

--- boto3test.py
+++ boto3test.py
@@ -1,23 +1,23 @@
 import os
 import boto3
-from awskeys import keyID, secretKey, token
+# from awskeys import keyID, secretKey, token
 
 session = boto3.Session(
-aws_access_key_id='ASIAVCS4GBQG6GZINNHC',
-aws_secret_access_key='s8xHn40ztunw/80JROvdBFvbepLZ6GQgDtCB7w0B',
-aws_session_token='FQoGZXIvYXdzEDsaDAevx5sG2Wf7+REO7CL7AhgSWcUmLX9Kx0UFnTiitLkDuj0htKx/3y72i86Leyxo4w+z3jRhWdfiKIwRbuDQS1hK0wuc0TeUSfn1wf1/6pknIxpJNG/8VUwbu81VnzAhaaK2YTFJHGnTN0ve/5cQaKkDTwmltNo9pgJ3IAvdxjD/zQOtG4w22mVDU+IU6lxYrye87clgvZQpspX1/+HG6/vetnH3La5UQCERQYO+cLXK2S+Cegh3VOmqNSgBxU8gsI7KGm1PGxMJg06tEaxO7iW45eQ56ZNjriOiDD11NplF0YZbSgwHcH1zXG8yc4aNgf0oM2z8zzZ/9WRC4468UXcRanHn1f++85Vewl0qCOXjWz5XZfPPZlZXxF+7ef0oFrxZ3V7Y2DlPCkKAHRuYPF8T/b+ZX5dlj/KzfvjcD4lRA+nOdCvZPDdZJ1agiFVjq2rHQsZ5zKHTGEWk3FKUnDHwxFC4XwiXNjX5E2qjseUVavabFOQAorQ+F8zUbjYmg6FlWOASaMn+zy4oyLSk6AU=',
+aws_access_key_id='ASIAVCS4GBQG4G3YSPNN',
+aws_secret_access_key='JBc1IxGs6xEmO6VK/hQZS2vHM6qIYF0uH5Xp33qF',
+aws_session_token='FQoGZXIvYXdzEDwaDOxevpsVUeqtSVnJkSL7AkDgB0tG3di+Vpozzv/evLNf13bQaralhW6kMS/K9cx3fQl6Yc3/D8dndAXFh+Ko0Skx7ddmtSCHGpzlegLgIUAXHkGehgbzh4inE/l0TBsZLjWUosaIJasciffjRWOY3UizWfxVawTnry7jws0/z+3mT5O64fSicvZ9qYo7HSssaKpCxU1wy9Q5NsW4lnAkS24xUq34m2u13aepxSE4pfxCDql5Mw1t41s+dqlTFvdHHgnwD0+a535ADkHhalSGfeGI0zELA1bs4KghgBWNH9HiNdbUZ9j6/+pO/CVrM3VjIBPm6+ppSncZXojufqpBT6e2pIxmC5XfznbQ9vAlkdISf0A1QAZx6K+5v/GTkVjRDNF15DuGlCKAhr2G7AqiTuLQ1LnRlZ1jh8SO0YkUcBADmvsOc729yOwxZS0lwkG/xZCqS4BqKxjxurT4vTiyDUCOZotu0Ui+VrgYvJL07fyqhRrisWCycCX3eOWaJpDf+WmX4BvnI2rcZKgooNmk6AU=',
 )
 
 s3 = session.resource('s3')
-
 for bucket in s3.buckets.all():
     print(bucket.name)
 
-def createUserNameFile(userName):
+
     userinput = input('Enter your username ')
-    f = open('username.txt', 'w')
+    f = open('username.txt', 'w+')
     f.write(userinput)
     f.close()
+    
+data = open('username.txt','rb')
+s3.Bucket('bucket-ishan747').put_object(Key='username.txt', Body=data)
 
-
-s3.Bucket('bucket-ishan747').put_object(Key='username.txt')
